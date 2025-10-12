e4e012c9f73e2273095b37f6957d454a996aeed8
Rolandas <rzabulis@yahoo.co.uk>
2025-10-12 23:15:59 +0300

    Dev: create Company Edit

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\Country;
 use Illuminate\Http\Request;
 
@@ -45,9 +46,9 @@ class CompanyController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Company $company)
     {
-        //
+        return view('companies.edit', compact('company'));
     }
 
     /**
