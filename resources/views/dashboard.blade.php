f7878a0404f371f16ca57821655d500f8e40f25f
Rolandas <rzabulis@yahoo.co.uk>
2025-10-13 18:43:04 +0300

    Dev: add nav in Dashboard with wire:navigate

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,19 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                <div class="p-6">
+                    <nav class="flex flex-col space-y-2">
+                        <a href="/post/2" wire:navigate class="block p-3 rounded-lg border border-gray-300 hover:bg-gray-100">
+                            Add comment to a Post
+                        </a>
+                        <a href="/companies/create" wire:navigate class="block p-3 rounded-lg border border-gray-300 hover:bg-gray-100">
+                            Create a Company
+                        </a>
+                        <a href="/locations/create" wire:navigate class="block p-3 rounded-lg border border-gray-300 hover:bg-gray-100">
+                            Create a Location
+                        </a>
+                    </nav>
+                </div>
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
